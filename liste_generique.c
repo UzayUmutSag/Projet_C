57aedcba3344e27e875904b313bcd329708d5dbf
NouboussiL <lionel.nouboussi@gmail.com>
2018-10-03 17:10:16 +0200

    ajout des fichiers des listes et noeuds génériques (par la prof)

---
+++ liste_generique.c
@@ -0,0 +1,65 @@
+//
+// Created by sophie on 29/08/18.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "noeud_generique.h"
+#include "liste_generique.h"
+
+lst creer_liste(void(*_copier)(void*, void**), void(*_detruire)(void**), void(*_afficher)(void*)) {
+    lst l = (lst)malloc(sizeof(struct liste));
+    l->tete = NULL;
+    l->queue = NULL;
+    l->copier = _copier;
+    l->detruire = _detruire;
+    l->afficher = _afficher;
+    l->taille = 0;
+    return l;
+}
+
+void ajouter_liste_fin(void* _val,lst l) {
+    nd n = creer_noeud(_val,l->copier);
+    if (l->tete==NULL) {
+        l->tete = n;
+        l->queue = n;
+    }
+    else {
+        (l->queue)->suivant = n;
+        (l->queue) = (l->queue)->suivant;
+    }
+    l->taille++;
+}
+
+void supprimer_liste_fin(lst l){
+    nd tmp = l->tete;
+    if (l->tete==l->queue)
+        detruire_liste(&l);
+    else {
+        while (tmp->suivant != l->queue)
+            tmp = tmp->suivant;
+        detruire_tout(&(l->queue), l->detruire);
+        tmp->suivant = NULL;
+        l->queue = tmp;
+    }
+}
+void detruire_liste(lst* l) {
+    detruire_tout(&((*l)->tete),(*l)->detruire);
+    free(*l);
+    (*l)=NULL;
+}
+void afficher_liste(lst l) {
+    afficher_tout(l->tete,l->afficher);
+}
+
+void* rechercher_max_liste(lst l,int(*_comparer)(void* _val1, void* _val2)) {
+    nd tmp = l->tete;
+    void *max = tmp->val;
+    for (int i = 1; i < l->taille; i++) {
+      tmp = tmp->suivant;
+      int cmp = _comparer(tmp->val,max);
+      if ( cmp >= 1)
+	max = tmp->val;
+    }
+    return max;
+}
---
+++ liste_generique.h
@@ -0,0 +1,28 @@
+//
+// Created by sophie on 29/08/18.
+//
+
+#ifndef UNTITLED_LISTE_GENERIQUE_H
+#define UNTITLED_LISTE_GENERIQUE_H
+
+struct liste {
+    int taille;
+    nd tete;
+    nd queue;
+    void(*copier)(void*, void**);
+    void(*detruire)(void**);
+    void(*afficher)(void*);
+};
+
+typedef struct liste* lst;
+
+lst creer_liste(void(*_copier)(void*, void**), void(*_detruire)(void**), void(*_afficher)(void*));
+
+void ajouter_liste_fin(void* _val,lst l);
+void supprimer_liste_fin(lst l);
+void detruire_liste(lst* l);
+void afficher_liste(lst l);
+
+void* rechercher_max_liste(lst l,int(*_comparer)(void* _val1, void* _val2));
+
+#endif //UNTITLED_LISTE_GENERIQUE_H
